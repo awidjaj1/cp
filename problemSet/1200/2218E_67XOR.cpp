32467d4e8169c6af434f2cdc77d790beacaac1e9
Andrew Widjaja <awidjaja777@gmail.com>
2026-08-02 19:55:21 -0400

    2218E done

--- problemSet/1200/2218E_67XOR.cpp
+++ problemSet/1200/2218E_67XOR.cpp
@@ -5,7 +5,10 @@ using ll = long long;
 using vi = vector<int>;
 using vll = vector<ll>;
 
-
+/**
+ * No matter what order you pick the last element will always be a_i ^ a_j
+ * where i != j. so the problem reduces to finding the largest pairwise xor
+ */
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -16,6 +19,23 @@ int main() {
     int n;
     while(t--){
         cin >> n;
+
+        vi a(n);
+
+        for(auto &x: a){
+            cin >> x;
+        }
+
+        int ret = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                ret = max(ret, a[i] ^ a[j]);
+            }
+        }
+
+        cout << ret << endl;
+
+
         
     }
 
